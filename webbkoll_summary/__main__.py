c82613fad01de8de407197557bfcf04f23747d7a
REKSAR DSV <reksarka@gmail.com>
2020-02-22 11:07:47 +0200

    Raw getting cookies summary for http://smava.de

--- webbkoll_summary/__main__.py
+++ webbkoll_summary/__main__.py
@@ -1,9 +1,13 @@
+url = 'http://smava.de'
 import selenium.webdriver
-
-
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
+ONLINE_CHECKER = 'https://webbkoll.dataskydd.net/en'
+browser = selenium.webdriver.Chrome()
+browser.get(ONLINE_CHECKER)
+url_input = browser.find_element_by_css_selector('input[name="url"]')
+url_input.send_keys(url)
+url_input.submit()
+results = browser.find_element_by_id('results-header')
+summary = results.find_element_by_css_selector('.summary')
+cookies = summary.find_element_by_css_selector('li:nth-child(4) > strong')
+print(cookies.text)
+browser.quit()
