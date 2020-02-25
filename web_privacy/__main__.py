e714bac47bf9404ffd00d283fe68b89c9dcc0933
REKSAR DSV <reksarka@gmail.com>
2020-02-25 12:36:26 +0200

    Reading URLs from file.

--- web_privacy/__main__.py
+++ web_privacy/__main__.py
@@ -1,14 +1,22 @@
+import sys
 import privacy
 import report
 
 
-urls = (
-    'https://webbkoll.dataskydd.net/en',
-    'http://smava.de',
-    'https://www.artstation.com/jakubrozalski',
-    'https://yahoo.com',
-)
+def filename():
+    if len(sys.argv) > 1:
+        return sys.argv[1]
+    default = 'urls.txt'
+    print(f'URLs file is not specified, trying to read {default} at root')
+    return default
 
+
+def read_urls():
+    with open(filename()) as txt:
+        return txt.read().split()
+
+
+urls = read_urls()
 infolist = privacy.check(urls)
 issues = report.issues(infolist)
 for issue in issues:
