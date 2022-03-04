c0bef2efb0dc8fc2b09b911140d4c7f9187b5b0c
reksarka <reksarka@gmail.com>
2022-03-04 21:20:46 +0200

    Full summary for test URL.

--- webbkoll/spiders/summary.py
+++ webbkoll/spiders/summary.py
@@ -1,4 +1,6 @@
+import re
 from scrapy import Spider, FormRequest
+from w3lib.html import remove_tags
 
 
 class SummarySpider(Spider):
@@ -8,10 +10,11 @@ class SummarySpider(Spider):
     def parse(self, response):
         return FormRequest.from_response(
             response,
+            callback=summary,
             formdata={
                 'url': 'http://smava.de',
-            },
-            callback=summary)
+            }
+        )
 
 
 def summary(response):
@@ -19,12 +22,13 @@ def summary(response):
         'Checked URL': checked_url(response),
         'Final URL': final_url(response),
         'IP': ip(response),
-        'HTTPS by default': has_https(response),
+        'HTTPS by default': check_success(response, 1),
         'Cookies': cookies(response),
         'Third-party requests': third_party_requests(response),
-        'Content Security Policy (CSP)': csp(response),
-        'Referrer Policy': referrer_policy(response),
+        'Content Security Policy (CSP)': check_success(response, 2),
+        'Referrer Policy': check_success(response, 3),
     }
+
 
 def checked_url(response):
     return response.css('.url li:first-child a::text').get()
@@ -32,29 +36,34 @@ def checked_url(response):
 def final_url(response):
     return response.css('.url li:last-child a::text').get()
 
-def ip(response):
-    # TODO:
-    pass
-
-def has_https(response):
-    return 'success' in response \
-        .css('.summary li:first-child i') \
-        .attrib['class']
-
 def cookies(response):
-    # TODO:
-    pass
+    cookies_re = '\d.*'
+    cookies_txt = summary_li(response, 4).get()
+    return find_line(cookies_re, cookies_txt)
 
 def third_party_requests(response):
-    # TODO:
-    pass
+    requests_re = '\d.*'
+    requests_txt = summary_li(response, 5).get()
+    return find_line(requests_re, requests_txt)
 
-def csp(response):
-    return 'success' in response \
-        .css('.summary li:nth-child(2) i') \
-        .attrib['class']
+def ip(response):
+    ip_re = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+    ip_txt = summary_li(response, 6).get()
+    return find_line(ip_re, ip_txt)
 
-def referrer_policy(response):
-    return 'success' in response \
-        .css('.summary li:nth-child(3) i') \
-        .attrib['class']
+def check_success(response, li_idx):
+    return 'success' in summary_li(response, li_idx).css('i').attrib['class']
+
+def summary_li(response, li_idx):
+    return response.css(f".summary li:nth-child({li_idx})")
+
+def normalize_txt(txt):
+    space = ' '
+    multispace = '\s{2,}'
+    oneline = txt.replace('\n', space)
+    return re.sub(multispace, space, oneline)
+
+def find_line(line_re, txt):
+    oneline = normalize_txt(remove_tags(txt))
+    match = re.search(line_re, oneline)
+    return match.group(0) if match else ''
