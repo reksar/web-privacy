a918d9cb4d255925a9b93a47f18837ae9572e3d0
reksarka <reksarka@gmail.com>
2022-04-21 17:54:52 +0300

    Accepts URLs to parse in the `start_urls` list.

--- webbkoll/spiders/summary.py
+++ webbkoll/spiders/summary.py
@@ -1,16 +1,29 @@
 from scrapy import Spider, FormRequest
+from scrapy.http import Request, Response
 from webbkoll.loaders import SummaryLoader
 from webbkoll.settings import WEBBKOLL_URL
 
 
 class SummarySpider(Spider):
     name = 'summary'
-    start_urls = [WEBBKOLL_URL]
+    summary = SummaryLoader()
+    start_urls = ['http://smava.de']
+
+    def start_requests(self):
+        yield from map(webbkoll_check, self.start_urls)
 
     def parse(self, response):
-        return FormRequest.from_response(
-            response,
-            # TODO: set URLs to parse.
-            formdata={'url': 'http://smava.de'},
-            callback=SummaryLoader(),
-        )
+        return self.summary(response)
+
+
+def webbkoll_check(url):
+    return Request(WEBBKOLL_URL,
+        callback=submit_check_form,
+        cb_kwargs={'url': url},
+        dont_filter=True)
+
+def submit_check_form(check_form: Response, **form_data: dict) -> Request:
+    return FormRequest.from_response(
+        check_form,
+        formdata=form_data,
+        dont_filter=True)
