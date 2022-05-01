361457c3242a9d9fdecf62b955634e68de6cfacd
reksarka <reksarka@gmail.com>
2022-05-01 16:38:33 +0300

    Drops an Item if can't parse the summary.

--- webbkoll/spiders/summary.py
+++ webbkoll/spiders/summary.py
@@ -1,5 +1,7 @@
 from scrapy import Spider
+from scrapy.exceptions import DropItem
 from webbkoll.loaders import SummaryLoader
+from w3lib.html import remove_tags
 
 
 class SummarySpider(Spider):
@@ -12,4 +14,15 @@ class SummarySpider(Spider):
         self.start_urls = kwargs.get('urls', '').split()
 
     def parse(self, response):
-        return self.summary(response)
+
+        if has_summary(response):
+            return self.summary(response)
+
+        raise DropItem(error_message(response))
+
+
+def has_summary(response):
+    return bool(len(response.css('#results-header')))
+
+def error_message(response):
+    return remove_tags(response.css('.text').re_first('<p>Error:.*</p>'))
